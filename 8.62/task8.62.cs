be0430fae99ede1522f52d7592234e4fbdd9ea2d
SectorNN <net.laifa@gmail.com>
2022-09-03 16:52:10 +0300

    Добавлено 8.62

---
+++ 8.62/task8.62.cs
@@ -0,0 +1,72 @@
+﻿// Доп. задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int[,] FillArray(int nr = 5, int nc = 5)                    // Функция заполняет массив змейкой
+{
+    int[,] arr = new int[nr, nc];
+    string direction = "r";
+    int val = 1;
+    int r = 0;
+    int c = -1;
+    int newC = c;
+    int newR = r;
+    do
+    {
+        switch (direction)
+        {
+            case "r": newC = c + 1; break;
+            case "d": newR = r + 1; break;
+            case "l": newC = c - 1; break;
+            case "u": newR = r - 1; break;
+        }
+        try
+        {
+            if (arr[newR, newC] != 0)
+            {
+                ChangeDirection(ref direction);
+                    newR = r; newC = c;
+                    continue;
+            }
+            arr[newR, newC] = val;
+        }
+        catch
+        {
+            ChangeDirection(ref direction);
+            newR = r; newC = c;
+            val--;
+        }
+        c = newC; r = newR;
+        val++;
+    }
+    while (val < nr * nc + 1);
+    return arr;
+}
+
+void ChangeDirection(ref string direction)                  // Функция меняет направление заполнения массива
+{
+    switch (direction)
+    {
+        case "r": direction = "d"; break;
+        case "d": direction = "l"; break;
+        case "l": direction = "u"; break;
+        case "u": direction = "r"; break;
+    }
+}
+
+void PrintArr(int[,] arr)                                   // Функция выводит двухмерный массив
+{
+    for (int x = 0; x < arr.GetLength(0); x++)
+    {
+        for (int y = 0; y < arr.GetLength(1); y++)
+        {
+            Console.Write($"{arr[x, y]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArr(FillArray(10, 10));
--- temp/Program.cs
+++ temp/Program.cs
@@ -1,105 +1,12 @@
-﻿// Задача 1: Задайте двумерный массив случайных чисел от 0 до 10. Напишите программу, которая поменяет местами 
-// первую и последнюю строку массива.
+﻿Console.WriteLine("Введите элементы массива через запятую: ");
+string [] arr = Console.ReadLine().Split(",");
+List<int> vals = new List<int>();
 
-// Например, задан массив:
-
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-
-// В итоге получается вот такой массив:
-
-// 8 4 2 4
-// 5 9 2 3
-// 1 4 7 2
-
-Console.Clear();
-int[,] GenerateDualArr(int nx = 5, int ny = 6)          // Функция генерирует двухмерный массив
-{
-    int[,] dualArr = new int[nx, ny];
-    Random rnd = new Random();
-    for (int x = 0; x < nx; x++)
+for (int i = 0; i < arr.Length; i++){
+    if (int.Parse(arr[i]) % 2 == 0)
     {
-        for (int y = 0; y < ny; y++)
-        {
-            dualArr[x, y] = rnd.Next(0, 10);
-        }
-    }
-    return dualArr;
-}
-
-void PrintArr(int[,] arr)                               // Функция выводит двухмерный массив
-{
-    for (int x = 0; x < arr.GetLength(0); x++)
-    {
-        for (int y = 0; y < arr.GetLength(1); y++)
-        {
-            Console.Write($"{arr[x, y]}\t");
-        }
-        Console.WriteLine();
+        vals.Add(int.Parse(arr[i]));
     }
 }
 
-void ReverseRows(int[,] arr)
-{
-    int [,] tmpArr = new int [arr.GetLength(0), arr.GetLength(1)];
-        for (int y = 0; y < arr.GetLength(1); y++)
-        {
-            tmpArr[0, y] = arr[0, y];
-            arr[0, y] = arr[arr.GetLength(0) - 1, y];
-            arr[arr.GetLength(0) - 1, y] = tmpArr[0, y];
-        }
-}
-
-int[,] arr = GenerateDualArr();
-PrintArr(arr);
-Console.WriteLine();
-ReverseRows(arr);
-PrintArr(arr);
-
-int [,] RemoveRowCol (int[,] arr)
-{
-    int minR = 0;
-    int minC = 0;
-    int minVal = arr[0, 0];
-    int useCOffset = 0;
-    int useROffset = 0;
-    int[,] newArr = new int[arr.GetLength(0) - 1, arr.GetLength(1) -1];
-    for (int r = 0; r < arr.GetLength(0); r++)
-    {
-        for (int c = 0; c < arr.GetLength(1); c++)
-        {
-            if (arr[r, c] < minVal)
-            {
-                minVal = arr[r, c];
-                minR = r;
-                minC = c;
-            }
-        }
-    }
-    for (int r = 0; r < arr.GetLength(0); r++)
-    {
-        useCOffset = 0;
-        if (r == minR)
-        {
-            useROffset = 1;
-            continue;
-        }
-        for (int c = 0; c < arr.GetLength(1); c++)
-        {
-            if (c == minC) 
-            {
-                useCOffset = 1;
-                continue;
-            }
-            newArr[r - useROffset, c - useCOffset] = arr[r, c];
-        }
-    }
-    return newArr;
-}
-
-Console.WriteLine();
-PrintArr(RemoveRowCol(arr));
-
-// Задача 2: Из двумерного массива случайных целых чисел от 0 до 10 удалить строку и столбец, на 
-// пересечении которых расположен наименьший элемент.
+Console.Write(string.Join(" ", vals));
